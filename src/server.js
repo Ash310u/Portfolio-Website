54a548a112e16e6b8d12d23e54a8036521bcfad0
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-07-05 14:32:57 +0530

    Commited portfolio website eith express and dynamic partials

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const hbs = require('hbs')
+const path = require('path')
+
+// calling express js
+const app = express()
+
+// Define paths for Express config
+const publicDir =  path.join(__dirname,'../public')
+const viewsPath =  path.join(__dirname,'../templates/views')
+const partialsPath =  path.join(__dirname,'../templates/partials')
+
+// Setup ejs engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDir))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.get('/about', (req, res) => {
+    res.render('index')
+})
+app.get('/work', (req, res) => {
+    res.render('index')
+})
+app.get('/blogs', (req, res) => {
+    res.render('index')
+})
+app.get('/contact', (req, res) => {
+    res.render('index')
+})
+
+app.listen(5000, () => {
+    console.log('server is up on port 5000.');
+})
