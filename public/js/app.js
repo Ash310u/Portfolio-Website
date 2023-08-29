154eb49cacb84fd05787bc68943d86cc1f9453b4
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-30 02:45:18 +0530

    Commited final version of cursor design using css and js

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,9 @@
 let mouseCursor = document.querySelector('.cursor')
-let navLinks =  document.querySelectorAll('#myLinks .yep')
                                     // here e is and event
 window.addEventListener('mousemove', (e) => {
     mouseCursor.style.top = `${e.pageY}px`;
     mouseCursor.style.left = `${e.pageX}px`;
 })
-
-navLinks.forEach( link => {
-    link.addEventListener('mouseleave', () => {
-        mouseCursor.classList.remove('link-grow')
-    })
-    link.addEventListener('mouseover', () => {
-        mouseCursor.classList.add('link-grow')
-    })
-});
 
 //    Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon 
 function myFunction() {
