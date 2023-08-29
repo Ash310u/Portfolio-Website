fd5dd2478480aa8d47a3c2ef4ea732fdd4bb6d10
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-30 01:46:23 +0530

    Created a mouse Follower with some animaton

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,24 @@
+let mouseCursor = document.querySelector('.cursor')
+let navLinks =  document.querySelectorAll('#myLinks .yep')
+                                    // here e is and event
+window.addEventListener('mousemove', (e) => {
+    mouseCursor.style.top = `${e.pageY}px`;
+    mouseCursor.style.left = `${e.pageX}px`;
+})
+
+navLinks.forEach( link => {
+    link.addEventListener('mouseleave', () => {
+        mouseCursor.classList.remove('link-grow')
+    })
+    link.addEventListener('mouseover', () => {
+        mouseCursor.classList.add('link-grow')
+    })
+});
 
 //    Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon 
 function myFunction() {
     if (window.matchMedia("(max-width: 767px)").matches) {
-        var x = document.getElementById("myLinks");
+        let x = document.getElementById("myLinks");
         if (x.style.display === "block") {
             x.style.display = "none";
         } else {
