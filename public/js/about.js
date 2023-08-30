d91e1b6b7ab239c58f9272c8ad2d6aa27d7917ed
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-31 00:02:21 +0530

    Commited about page

---
+++ public/js/about.js
@@ -0,0 +1,15 @@
+// toggle the search button 
+function toggle() {
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active");
+    var popup = document.getElementById("popup");
+    popup.classList.toggle("active");
+}
+
+// let search = document.getElementById("input").value;
+
+// let result = search.find(function (arg) {
+//     return arg.search
+//   })
+
+//   console.log(result);
--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 app.get('/about', (req, res) => {
-    res.render('index')
+    res.render('about')
 })
 app.get('/work', (req, res) => {
     res.render('index')
